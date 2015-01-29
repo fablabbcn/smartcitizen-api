023b23dc608b9b159f4123ca6bf2b057b63d7ddf
John Rees <john@bitsushi.com>
2015-01-29 15:01:37 +0100

    home

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
   get "/404" => "errors#not_found"
   get "/500" => "errors#exception"
 
-  api_version(module: "V0", path: {value: "v0"}) do
+  api_version(module: "V0", path: {value: "v0"}, default: true) do
     resources :users
     resources :devices
+    root to: 'static#home'
   end
+  root to: 'v0/static#home'
 
 end
