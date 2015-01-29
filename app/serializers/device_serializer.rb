39ddc0720cd9af8d9e75b5a46ed9d82b944eedd4
John Rees <john@bitsushi.com>
2015-01-29 16:16:40 +0100

    cass

--- app/serializers/device_serializer.rb
+++ app/serializers/device_serializer.rb
@@ -1,9 +1,9 @@
 class DeviceSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :latitude, :longitude, :latest_readings
+  attributes :id, :name, :description, :latitude, :longitude#, :latest_readings
   has_one :owner
 
 
-  def latest_readings
-    Reading.select(:value).where(device_id: id).first
-  end
+  # def latest_readings
+  #   Reading.select(:value).where(device_id: id).first
+  # end
 end
