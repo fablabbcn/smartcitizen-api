9f0473e3a6e8a45fe50b14ddaa679d96add697e4
John Rees <john@bitsushi.com>
2015-02-02 17:54:27 +0100

    pretty json :lipstick:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+
 require 'bouncer'
 
 class ApplicationController < ActionController::API
@@ -6,6 +7,22 @@ class ApplicationController < ActionController::API
   include ActionController::Serialization
   include ActionController::HttpAuthentication::Basic::ControllerMethods
   include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  # http://stackoverflow.com/a/23018176
+  ActionController::Renderers.add :json do |json, options|
+    unless json.kind_of?(String)
+      json = json.as_json(options) if json.respond_to?(:as_json)
+      json = JSON.pretty_generate(json, options)
+    end
+
+    if options[:callback].present?
+      self.content_type ||= Mime::JS
+      "#{options[:callback]}(#{json})"
+    else
+      self.content_type ||= Mime::JSON
+      json
+    end
+  end
 
   force_ssl if: :ssl_configured?
 
