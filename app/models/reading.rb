f1623ae250f7ec78fd8efa6b4cafd8e29eb81375
John Rees <john@bitsushi.com>
2015-02-11 18:46:13 +0100

    heroku tweakg

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -13,16 +13,16 @@ class Reading
     Device.find(device_id)
   end
 
-  def self.for_device_id device_id
-    months = [2015,2014].collect{|y| ("#{y}01".to_i.."#{y}12".to_i).to_a.reverse }.join(',')
-    $cassandra.execute("SELECT recorded_at, values FROM readings
-      WHERE device_id = #{device_id} AND recorded_month IN (#{months})")
-  end
-
   def self.add device_id, recorded_at, values
     values = values.sort.to_h.to_json.gsub(/\"/,"'")
     $cassandra.execute("INSERT INTO readings(device_id, recorded_month, recorded_at, values)
       VALUES (#{device_id}, #{recorded_at.strftime('%Y%m')}, '#{recorded_at.iso8601}', #{values})")
+  end
+
+  def self.for_device_id device_id
+    months = [2015,2014].collect{|y| ("#{y}01".to_i.."#{y}12".to_i).to_a.reverse }.join(',')
+    $cassandra.execute("SELECT recorded_at, values FROM readings
+      WHERE device_id = #{device_id} AND recorded_month IN (#{months})")
   end
 
 end
