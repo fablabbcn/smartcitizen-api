6f5afa79ccfa1f5ded7c259b822e61e0f09a3db0
John Rees <john@bitsushi.com>
2015-03-03 09:49:33 +0000

    reading

--- spec/requests/v0/readings_spec.rb
+++ spec/requests/v0/readings_spec.rb
@@ -10,14 +10,12 @@ describe V0::ReadingsController do
   end
 
   describe "GET /add" do
-
     it "returns time" do
       Timecop.freeze(Time.utc(2015,02,01,20,00,05)) do
         get "/add"
         expect(response.body).to eq("UTC:2015,2,1,20,00,05#")
       end
     end
-
   end
 
   skip "has legacy push support" do
