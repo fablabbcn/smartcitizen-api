aed59870865a249bc9d3ba505bf583dac8420ee8
John Rees <john@bitsushi.com>
2015-03-31 01:17:37 +0100

    added city back

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :username, :device_ids, :country_code, :joined_at#, :updated_at :city
+  attributes :username, :device_ids, :country_code, :city, :joined_at#, :updated_at
 end
