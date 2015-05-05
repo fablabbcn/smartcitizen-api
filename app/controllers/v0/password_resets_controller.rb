cddef157963f121e5db0c4215502db7baac5dc94
John Rees <john@bitsushi.com>
2015-05-05 11:20:05 +0100

    refactor password_resets

--- app/controllers/v0/password_resets_controller.rb
+++ app/controllers/v0/password_resets_controller.rb
@@ -1,13 +1,6 @@
 module V0
   class PasswordResetsController < ApplicationController
 
-    def show
-      @user = User.find_by!(password_reset_token: params[:id])
-      @current_user = @user
-      authorize @user, :update_password?
-      render json: @user, status: :ok
-    end
-
     def create
       @user = User.find_by!(username: params.require(:username))
       authorize @user, :request_password_reset?
@@ -15,16 +8,21 @@ module V0
       render json: {message: 'Password Reset Instructions Delivered'}, status: :ok
     end
 
+    def show
+      @user = User.find_by!(password_reset_token: params[:id])
+      @current_user = @user
+      authorize @user, :update_password?
+      render json: @user, status: :ok
+    end
+
     def update
       @user = User.find_by!(password_reset_token: params[:id])
       @current_user = @user
       authorize @user, :update_password?
-      @current_user = nil
       if @user.update_attributes({ password: params.require(:password), password_reset_token: nil })
         render json: @user, status: :ok
       else
         raise Smartcitizen::UnprocessableEntity.new @user.errors
-      #   render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
       end
     end
 
