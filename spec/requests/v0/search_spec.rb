9ae7b00f20896587ecb77040b00d3442ea4f52a5
John Rees <john@bitsushi.com>
2015-06-01 20:59:17 +0100

    search specs

---
+++ spec/requests/v0/search_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe V0::StaticController do
+
+  describe "search" do
+
+    it "can search by location" do
+      liverpool = create(:device, city: "liverpool", latitude: 53.419906, longitude: -2.9108462)
+      manchester = create(:device, city: "manchester", latitude: 53.4722454, longitude: -2.2235922)
+
+      api_get "/search?q=manchester"
+
+      expect(response.status).to eq(200)
+      expect(JSON.parse(response.body)[0]["id"]).to eq(manchester.id)
+    end
+
+    it "can search by user's first_name" do
+      marge = create(:user, first_name: 'marge')
+      bart = create(:user, first_name: 'bart')
+
+      api_get "/search?q=bart"
+
+      expect(response.status).to eq(200)
+      expect(JSON.parse(response.body)[0]["id"]).to eq(bart.id)
+    end
+
+  end
+
+end
