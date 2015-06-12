480ec9068b1850555409395bd5d6203781bd4b81
John Rees <john@bitsushi.com>
2015-06-12 18:18:06 +0100

    all intervals

--- app/controllers/v0/pg_readings_controller.rb
+++ app/controllers/v0/pg_readings_controller.rb
@@ -9,26 +9,52 @@ module V0
 
     def index
       @device = Device.find(params[:device_id])
-      sensors = @device.sensors.order('sensors.id ASC').pluck(:id).map{ |id| "avg((data->>'#{id}')::numeric) AS sensor_#{id}" }.join(', ')
+      sensor_ids = @device.sensors.order('sensors.id ASC').pluck(:id)
+      sensors = sensor_ids.map{ |id| "avg((data->>'#{id}')::numeric) AS sensor_#{id}" }.join(', ')
 
       PgReading.select("date_trunc('day', recorded_at) AS day, #{sensors}").group("1").order("1")
 
-      select = "SELECT date_trunc('day', recorded_at)::timestamp without time zone AS day"
-      rollup = '1d'
-      from = params[:from] ? Time.parse(params[:from]) : 1.month.ago
+      to = params[:to] ? Time.parse(params[:to] + " UTC") : Time.now
+      readings = []
+      data = Hash[sensor_ids.map {|key, value| [key, nil]}]
 
       if params[:rollup] == '1h'
         select = "SELECT date_trunc('hour', recorded_at)::timestamp without time zone AS day"
         rollup = '1h'
         from = params[:from] ? Time.parse(params[:from]) : 1.week.ago
+        if params[:all_intervals]
+          time_iterate(from.beginning_of_hour, to.end_of_hour, 1.hour) do |t|
+            readings << {
+              timestamp: t,
+              data: data
+            }
+          end
+        end
       elsif params[:rollup] == '1m'
         select = "SELECT date_trunc('minute', recorded_at)::timestamp without time zone AS day"
         rollup = '1m'
         from = params[:from] ? Time.parse(params[:from]) : 1.hour.ago
+        if params[:all_intervals]
+          time_iterate(from.beginning_of_minute, to.end_of_minute, 1.minute) do |t|
+            readings << {
+              timestamp: t,
+              data: data
+            }
+          end
+        end
+      else # day
+        select = "SELECT date_trunc('day', recorded_at)::timestamp without time zone AS day"
+        rollup = '1d'
+        from = params[:from] ? Time.parse(params[:from]) : 1.month.ago
+        if params[:all_intervals]
+          time_iterate(from.beginning_of_day, to.end_of_day, 1.day) do |t|
+            readings << {
+              timestamp: t,
+              data: data
+            }
+          end
+        end
       end
-
-
-      to = params[:to] ? Time.parse(params[:to] + " UTC") : Time.now
 
       sql = %{
         SET TIME ZONE 'UTC';
@@ -43,14 +69,17 @@ module V0
 
       @pg_readings = ActiveRecord::Base.connection.execute(sql)
 
-      ob = { rollup: rollup, function: 'average', from: from, to: to, readings: [nil] }
+      ob = { rollup: rollup, function: 'average', from: from, to: to, readings: readings }
+
       @pg_readings.each do |reading|
-        ob['readings'] ||= []
-        ob['readings'] << {
+        ob[:readings] = ob[:readings] - ob[:readings].select{|hash| hash[:timestamp] == Time.parse(reading['day'] + " UTC") }
+        ob[:readings] << {
           timestamp: Time.parse(reading['day'] + " UTC"),
           data: reading.select { |key, value| key.to_s.match(/^sensor_\d+/) }.map{|k,v| [k.gsub('sensor_',''), to_f_or_i_or_s(v)] }.to_h
         }
       end
+
+      ob[:readings].sort_by!{|a| a[:timestamp]}.reverse!
 
       render json: ob
 
@@ -59,6 +88,15 @@ module V0
       # @pg_readings = @device.pg_readings
       # @pg_readings = paginate(@pg_readings)
     end
+
+private
+
+  def time_iterate(start_time, end_time, step, &block)
+    begin
+      yield(start_time)
+    end while (start_time += step) <= end_time
+  end
+
 
   end
 end
