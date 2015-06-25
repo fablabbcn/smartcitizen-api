543d452413f7da25e8d213578090b81526dffbf5
John Rees <john@bitsushi.com>
2015-06-25 16:24:30 +0100

    cors

--- config/application.rb
+++ config/application.rb
@@ -46,7 +46,7 @@ module Smartcitizen
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
 
