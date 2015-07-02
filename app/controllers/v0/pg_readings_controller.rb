949f5c5068402d515b0e4aa95052102a47f16952
John Rees <john@bitsushi.com>
2015-07-02 16:21:38 +0100

    comment out 30m

--- app/controllers/v0/pg_readings_controller.rb
+++ app/controllers/v0/pg_readings_controller.rb
@@ -30,6 +30,19 @@ module V0
             }
           end
         end
+      # elsif params[:rollup] == '30m'
+      #   select = "SELECT date_trunc('hour', recorded_at)::timestamp without time zone AS hour_stamp, (extract(minute FROM recorded_at)::int / 30) AS day"
+      #   rollup = '30m'
+      #   from = params[:from] ? Time.parse(params[:from]) : 1.week.ago
+      #   if params[:all_intervals]
+      #     time_iterate(from.beginning_of_hour, to.end_of_hour, 1.hour) do |t|
+      #       readings << {
+      #         timestamp: t,
+      #         data: data
+      #       }
+      #     end
+      #   end
+
       elsif params[:rollup] == '1m'
         select = "SELECT date_trunc('minute', recorded_at)::timestamp without time zone AS day"
         rollup = '1m'
