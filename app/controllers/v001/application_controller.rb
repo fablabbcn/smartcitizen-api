4d76afbc64a06c6288923455be0b498094c41829
John Rees <john@bitsushi.com>
2015-07-21 12:29:17 +0100

    401 if invalid key

--- app/controllers/v001/application_controller.rb
+++ app/controllers/v001/application_controller.rb
@@ -1,4 +1,16 @@
 module V001
   class ApplicationController < ActionController::API
+
+    before_action :check_api_key
+
+private
+
+    def check_api_key
+      begin User.find(params[:api_key])
+      rescue ActiveRecord::RecordNotFound
+        render json: {error: "invalid API KEY"}, status: :unauthorized
+      end
+    end
+
   end
 end
