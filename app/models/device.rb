65a00468f65daba8413f6197cf7ef6fa9adef990
John Rees <john@bitsushi.com>
2015-07-22 11:11:43 +0100

    heroku

--- app/models/device.rb
+++ app/models/device.rb
@@ -66,7 +66,7 @@ class Device < ActiveRecord::Base
     [
       exposure, # indoor / outdoor
       ('new' if created_at > 1.week.ago), # new
-      ((last_recorded_at > 10.minutes.ago ? 'online' : 'offline') if data) # state
+      ((data.recorded_at > 10.minutes.ago ? 'online' : 'offline') if data.present?) # state
     ].compact.sort
   end
 
