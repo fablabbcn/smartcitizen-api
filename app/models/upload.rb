e146aca6e2d385e055e2aabc86c80794b9e44128
John Rees <john@bitsushi.com>
2015-07-27 16:38:24 +0100

    https avatar

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -11,7 +11,7 @@ class Upload < ActiveRecord::Base
   end
 
   def full_path
-    "http://images.smartcitizen.me/s100/#{key}"
+    "https://images.smartcitizen.me/s100/#{key}"
   end
 
   def self.uploaded _key
