212be190c44b159020a8d4a7f7a3e392f2c575fc
John Rees <john@bitsushi.com>
2015-10-28 16:30:11 +0000

    places

--- app/controllers/v0/static_controller.rb
+++ app/controllers/v0/static_controller.rb
@@ -27,14 +27,14 @@ module V0
       begin
         url = "http://search.mapzen.com/v1/autocomplete?api_key=#{ENV['mapzen_api_key']}&text=#{CGI.escape(params[:q])}"
         data = JSON.parse(open(url).read)
-        data['features'].each do |feature|
+        data['features'].take(5).each do |feature|
           a << {
             type: "City",
             city: feature['properties']['name'],
             country_code: ISO3166::Country.find_country_by_alpha3(feature['properties']['country_a'].downcase).alpha2,
             country: feature['properties']['country'],
-            latitude: feature['geometry']['coordinates'][0],
-            longitude: feature['geometry']['coordinates'][1]
+            latitude: feature['geometry']['coordinates'][1],
+            longitude: feature['geometry']['coordinates'][0]
           }
         end
       rescue Exception => e
