f1d6349b23a48abea3ab6bdd6b9da0eef189d7c6
John Rees <john@bitsushi.com>
2015-10-30 13:32:21 +0000

    codeclimate

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,7 @@ require 'vcr'
 VCR.configure do |config|
   config.cassette_library_dir = "spec/vcr_cassettes"
   config.hook_into :webmock # or :fakeweb
+  config.ignore_hosts 'codeclimate.com'
 end
 
 
