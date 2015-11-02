03a3f23937f37aefb23c6268f5699d43036399a8
John Rees <john@bitsushi.com>
2015-11-02 10:40:00 +0000

    sidekiq monitoring @ /sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
+require "sidekiq/web"
+
 Rails.application.routes.draw do
+
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    username == ENV["sidekiq_username"] && password == ENV["sidekiq_password"]
+  end if Rails.env.production?
+  mount Sidekiq::Web, at: "/sidekiq"
 
   # resources :tags, except: [:new, :edit]
   api_version(module: "V001", path: {value: "v0.0.1"}, header: {name: "Accept", value: "application/vnd.smartcitizen; version=0.0.1"}, defaults: { format: :json }) do
