c17db3edfe18d190d81cbf1508c3f2ccdbd7ae18
John Rees <john@bitsushi.com>
2015-11-16 17:52:08 +0000

    add destroy to measurement specs

--- app/policies/measurement_policy.rb
+++ app/policies/measurement_policy.rb
@@ -5,7 +5,11 @@ class MeasurementPolicy < ApplicationPolicy
   end
 
   def create?
-    user and user.is_admin?
+    user.try(:is_admin?)
+  end
+
+  def destroy?
+    create?
   end
 
   def update?
