0d2ad6020bdf207d73afc70f6526768fc5145ce0
John Rees <john@bitsushi.com>
2015-11-16 21:37:51 +0000

    static spec done, but needs search specs

--- spec/requests/v0/static_spec.rb
+++ spec/requests/v0/static_spec.rb
@@ -2,14 +2,7 @@ require 'rails_helper'
 
 describe V0::StaticController do
 
-  describe "GET 404" do
-    it "returns 404 error" do
-      api_get '/404'
-      expect(response.status).to eq(404)
-      expect(response.body).to match("Endpoint not found")
-    end
-  end
-
+  # def home
   describe "GET /" do
     it "returns a list of routes" do
       json = api_get '/'
@@ -27,30 +20,11 @@ describe V0::StaticController do
     end
   end
 
+  # def search
   describe "search" do
     it "returns a list of searched objects" do
       api_get "/search?q=device"
       expect(response.status).to eq(200)
-    end
-  end
-
-  describe "format" do
-
-    it "(JSON) returns pretty JSON, with JSON Mimetype" do
-      json = api_get '/v0/kits'
-      expect( response.body.to_s ).to_not eq( JSON.pretty_generate(json) )
-      expect(response.header['Content-Type']).to include('application/json')
-    end
-
-    it "(JSON) returns pretty JSON, with JSON Mimetype if ?pretty=true" do
-      json = api_get '/v0/kits?pretty=true'
-      expect( response.body.to_s ).to eq( JSON.pretty_generate(json) )
-      expect(response.header['Content-Type']).to include('application/json')
-    end
-
-    it "(JSONP) returns JS Mimetype if callback param present" do
-      api_get '/v0/kits?callback=something'
-      expect(response.header['Content-Type']).to include('text/javascript')
     end
   end
 
