36d5107736fed7e8f3b4a144c10125fb9ad0865c
John Rees <john@bitsushi.com>
2015-11-24 19:10:03 +0000

    mathematician fix

--- lib/mathematician.rb
+++ lib/mathematician.rb
@@ -8,10 +8,17 @@ module Mathematician
   def self.reverse_table_calibration( arr, raw_value )
     raw_value = raw_value.to_f
     arr = Hash[arr.collect{|k,v| [v,k] }].to_a.sort!
+
+    if (raw_value >= arr.last[0])
+      return arr.last[1].to_f
+    end
+
+    clamped_value = [arr.first[0], raw_value, arr.last[0]-1].sort[1]
+
     for i in (0..arr.length-1)
-      if raw_value >= arr[i][0] && raw_value < arr[i+1][0]
+      if clamped_value >= arr[i][0] && clamped_value < arr[i+1][0]
         low, high = [arr[i], arr[i+1]]
-        return self.linear_regression(raw_value,low[1],high[1],arr[i][0],high[0])
+        return self.linear_regression(clamped_value,low[1],high[1],arr[i][0],high[0])
       end
     end
   end
@@ -19,6 +26,10 @@ module Mathematician
   def self.table_calibration( arr, raw_value )
     raw_value = raw_value.to_f
     arr = arr.to_a.sort!
+
+    if (raw_value >= arr.last[0])
+      return arr.last[1].to_f
+    end
 
     # new_value = [raw_value, [raw_value, arr.last[0]-1].min, arr.first[0]].max
     clamped_value = [arr.first[0], raw_value, arr.last[0]-1].sort[1]
