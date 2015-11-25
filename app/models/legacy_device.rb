3f2ebbc5acb7dd443d5e8b324f4da395f00e507f
John Rees <john@bitsushi.com>
2015-11-25 23:01:34 +0000

    geolatlng legacy

--- app/models/legacy_device.rb
+++ app/models/legacy_device.rb
@@ -29,6 +29,8 @@ class LegacyDevice < MySQL
     hash = {}
     cols = %w(id title description location city country exposure elevation title location geo_lat geo_long created last_insert_datetime)
     cols.map{ |c| hash[c] = self[c].to_s }
+    hash['geo_lat'] = nil if hash['geo_lat'] == ""
+    hash['geo_long'] = nil if hash['geo_long'] == ""
     # self['last_insert_datetime'] = Device.find(id).last_reading_at
     hash['last_insert_datetime'] = (hash['last_insert_datetime'] + " UTC").gsub(/( UTC)+/, " UTC")
 
