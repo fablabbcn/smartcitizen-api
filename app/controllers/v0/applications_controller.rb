a34cadd5a5c282c3490e74ca82959e2c6a7e954b
John Rees <john@bitsushi.com>
2016-04-12 10:25:21 +0100

    remove pundit auth for scoped applications

--- app/controllers/v0/applications_controller.rb
+++ app/controllers/v0/applications_controller.rb
@@ -2,7 +2,6 @@ module V0
   class ApplicationsController < ApplicationController
 
     before_action :check_if_authorized!
-    after_action :verify_authorized
 
     def index
       @applications = current_user.oauth_applications
