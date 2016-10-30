c8a17f0d1a226a0f20f7d7e7ef3ea1d1dd68cc54
Roger Pales <pales.roger@gmail.com>
2016-10-30 22:05:21 +0000

    orphan_device: device_token: can't be nil & attr_readonly

--- app/controllers/v0/onboarding/orphan_devices_controller.rb
+++ app/controllers/v0/onboarding/orphan_devices_controller.rb
@@ -13,8 +13,8 @@ module V0
 
         if save_orphan_device
           render json: {
-                          onboarding_session: orphan_device.onboarding_session,
-                          device_token: orphan_device.device_token
+                          onboarding_session: @orphan_device.onboarding_session,
+                          device_token: @orphan_device.device_token
                         },
                  status: :created
         else
@@ -36,16 +36,20 @@ module V0
         params.permit(:name, :description, :kit_id, :exposure, :latitude, :longitude, :user_tags)
       end
 
-      def onboarding_session
-        params.require(:onboarding_session)
-      end
-
       def set_orphan_device
-        @orphan_device = OrphanDevice.find_by(onboarding_session: onboarding_session)
+        @orphan_device = OrphanDevice.find_by(
+          onboarding_session: request.headers['HTTP_ONBOARDING_SESSION']
+        )
         render json: { error: 'Invalid onboarding_session' }, status: :not_found if @orphan_device.nil?
       end
 
       def save_orphan_device
+        @orphan_device.generate_device_token
+        @orphan_device.save!
+      rescue ActiveRecord::RecordInvalid => e
+        @attempts = @attempts.to_i + 1
+        retry if @attempts < 10
+        false
       end
     end
   end
