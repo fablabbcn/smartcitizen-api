c0b0c722b0164c14ddd9b3bb3a3611cddfa01c96
Roger Pales <pales.roger@gmail.com>
2016-10-30 20:32:51 +0000

    rewriting device_token

--- app/models/orphan_device.rb
+++ app/models/orphan_device.rb
@@ -1,13 +1,13 @@
 class OrphanDevice < ActiveRecord::Base
-  attr_readonly :onboarding_session
+  attr_readonly :onboarding_session, :device_token
 
   validates_uniqueness_of :device_token
   validates_uniqueness_of :onboarding_session
 
-  validate :device_token_persistance, on: :update
+  validate :device_token, presence: true, allow_nil: false
   validates :exposure, inclusion: { in: %w(indoor outdoor) }, allow_nil: true
 
-  after_create :generate_device_token
+  before_create :generate_device_token
   after_initialize :generate_onbarding_session
 
   TOKEN_ATTEMPTS = 10
@@ -24,26 +24,13 @@ class OrphanDevice < ActiveRecord::Base
     }
   end
 
+  def generate_device_token
+    self.device_token = SecureRandom.hex(3)
+  end
+
   private
 
   def generate_onbarding_session
     self.onboarding_session = SecureRandom.uuid if new_record?
-  end
-
-  def generate_device_token
-    self.update!(device_token: SecureRandom.hex(3))
-  rescue ActiveRecord::RecordInvalid => e
-    @attempts = @attempts.to_i + 1
-    retry if TOKEN_ATTEMPTS > @attempts
-    raise e, 'device_token assignment failed'
-  end
-
-  def device_token_persistance
-    return if device_token_was.nil? || !device_token_changed?
-    errors.add(:device_token,'cannot be changed')
-  end
-
-  def device_token_changed?
-    device_token_was != device_token
   end
 end
