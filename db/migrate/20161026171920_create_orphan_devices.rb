0f8a934735a785786c1e1569b69197dba1462e14
Roger Pales <pales.roger@gmail.com>
2016-11-01 12:54:33 +0000

    unique on db too

--- db/migrate/20161026171920_create_orphan_devices.rb
+++ db/migrate/20161026171920_create_orphan_devices.rb
@@ -8,10 +8,12 @@ class CreateOrphanDevices < ActiveRecord::Migration
       t.float :latitude
       t.float :longitude
       t.text :user_tags
-      t.string :device_token
+      t.string :device_token, null: false
       t.string :onboarding_session
 
       t.timestamps null: false
     end
+
+    add_index :orphan_devices, [:device_token], unique: true
   end
 end
