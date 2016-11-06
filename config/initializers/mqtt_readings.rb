4a7171cbf40254b75af08aadb34eb45ab82907ea
rpales <pales.roger@gmail.com>
2016-11-06 20:59:58 +0000

    use EMQ queue subscribe option

--- config/initializers/mqtt_readings.rb
+++ config/initializers/mqtt_readings.rb
@@ -3,7 +3,7 @@ Thread.new do
 
   EventMachine.run do
     EventMachine::MQTT::ClientConnection.connect(host: ENV['mqqt_host'], clean_session: true) do |c|
-      c.subscribe('device/sck/+/readings')
+      c.subscribe('$queue/device/sck/+/readings')
       c.receive_callback do |packet|
         MqttHandler::ReadingsPacket.store(packet)
       end
