d77411889f8bec6488958b9b622d4cd4cb48222b
rpales <pales.roger@gmail.com>
2016-11-10 20:40:34 +0000

    subscribe QoS = 1

--- config/initializers/mqtt.rb
+++ config/initializers/mqtt.rb
@@ -3,8 +3,10 @@ Thread.new do
 
   EventMachine.run do
     EventMachine::MQTT::ClientConnection.connect(host: ENV['mqtt_host'], clean_session: true) do |c|
-      c.subscribe('$queue/device/sck/+/readings')
-      c.subscribe('$queue/device/sck/+/hello')
+      c.subscribe({
+        '$queue/device/sck/+/readings' => 1,
+        '$queue/device/sck/+/hello' => 1
+      })
 
       c.receive_callback do |packet|
         MqttMessagesHandler.handle(packet)
