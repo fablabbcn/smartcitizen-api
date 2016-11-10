9497cd7e9ebcba5f872192fe7417df2efc29a7dd
Hugo Freire <hfreire@abajar.com>
2016-11-10 12:05:17 +0100

    Change mqqt_host env variable by mqtt_host

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  Figaro.require_keys('mqqt_host')
+  Figaro.require_keys('mqtt_host')
 else
-  ENV['mqqt_host'] = '127.0.0.1' if ENV['mqqt_host'].nil?
+  ENV['mqtt_host'] = '127.0.0.1' if ENV['mqtt_host'].nil?
 end
