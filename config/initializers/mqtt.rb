e5f23a18f7a39dbd9b2eb01be1ded949048533f7
rpales <pales.roger@gmail.com>
2016-11-10 14:37:41 +0000

    small issue

--- config/initializers/mqtt.rb
+++ config/initializers/mqtt.rb
@@ -1,5 +1,5 @@
 Thread.new do
-  EventMachine::error_handler { |e| puts Airbrake.notify(e) }
+  EventMachine::error_handler { |e| Airbrake.notify(e) }
 
   EventMachine.run do
     EventMachine::MQTT::ClientConnection.connect(host: ENV['mqqt_host'], clean_session: true) do |c|
