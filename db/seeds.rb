cf9896a93a0825acdc6734f8a709b5a49c9134b2
Viktor Smari <viktorsmari@gmail.com>
2018-02-13 09:10:42 +0100

    Create test seeds to use in local development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(
+  username: 'user1',
+  email: 'email@example.com',
+  password: 'password'
+)
+
+Kit.create(
+  name: 'kit',
+  description: 'my description'
+)
+
+Measurement.create(
+  name: 'my measure',
+  description: 'meas descr'
+)
+
+Sensor.create(
+  name: 'sensor1',
+  description: 'sens descript'
+)
+
+Tag.create(
+  name: 'tag1',
+  description: 'tag descript'
+)
+
+#ApiToken.create(
+#  owner_id: User.first,
+#  token: 'random token'
+#)
+
+#Component.create(
+#  board_id: 
+#)
+
+#device has many sensors through components
+Device.create(
+  owner_id: User.first,
+  name: 'device1',
+  description: 'device descript',
+  mac_address: 'macaddress',
+  latitude: 1.1,
+  longitude: 1.2,
+  kit_id: Kit.first
+)
+
+DevicesTag.create(
+  device: Device.first,
+  tag: Tag.first
+)
