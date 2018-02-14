286868a67e8f2efe91b5360e0772c453bb41215d
Viktor Smari <viktorsmari@gmail.com>
2018-02-14 12:59:20 +0100

    Remove Airbrake/errbit init file

--- config/initializers/errbit.rb
+++
@@ -1,6 +0,0 @@
-Airbrake.configure do |config|
-  config.api_key = ENV['errbit_api_key']
-  config.host    = ENV['errbit_host']
-  config.port    = ENV['errbit_port']
-  config.secure  = config.port == ENV['errbit_port']
-end
