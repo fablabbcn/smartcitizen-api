cf78cf05052d8ea399d27c59c06a0d9e1790ed5f
Viktor Smari <viktorsmari@gmail.com>
2018-03-02 13:26:55 +0100

    Move discourse spec under v0/ folder + fix indent

--- app/controllers/v0/discourse_controller.rb
+++ app/controllers/v0/discourse_controller.rb
@@ -1,28 +1,28 @@
 module V0
-class DiscourseController < ApplicationController
+  class DiscourseController < ApplicationController
 
-  before_action :check_if_authorized!
+    before_action :check_if_authorized!
 
-  def sso
-    secret = Figaro.env.discourse_sso_secret
-    sso = SingleSignOn.parse(request.query_string, secret)
-    sso.email = current_user.email # from devise
-    sso.name = current_user.full_name # this is a custom method on the User class
-    sso.username = current_user.email # from devise
-    #sso.username = current_user.username
-    sso.external_id = current_user.id # from devise
-    sso.sso_secret = secret
+    def sso
+      secret = Figaro.env.discourse_sso_secret
+      sso = SingleSignOn.parse(request.query_string, secret)
+      sso.email = current_user.email # from devise
+      sso.name = current_user.full_name # this is a custom method on the User class
+      sso.username = current_user.email # from devise
+      #sso.username = current_user.username
+      sso.external_id = current_user.id # from devise
+      sso.sso_secret = secret
 
-    redirect_to sso.to_url("#{Figaro.env.discourse_endpoint}session/sso_login")
-  rescue => e
-    Rails.logger.error(e.message)
-    Rails.logger.error(e.backtrace)
-    #flash[:error] = 'SSO error'
-    render inline: "Error, check logs"
+      redirect_to sso.to_url("#{Figaro.env.discourse_endpoint}session/sso_login")
+    rescue => e
+      Rails.logger.error(e.message)
+      Rails.logger.error(e.backtrace)
+      #flash[:error] = 'SSO error'
+      render inline: "Error, check logs"
 
-    #redirect_to "/"
-    #redirect_to root
+      #redirect_to "/"
+      #redirect_to root
+    end
+
   end
-
-end
 end
