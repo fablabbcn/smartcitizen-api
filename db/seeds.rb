8440cc36d151000aeb05c5526ffcfa4cd4112906
Viktor Smari <viktorsmari@gmail.com>
2018-03-06 13:32:07 +0100

    Seeds now create more random data for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,30 +16,81 @@ p '------ Seeding for development environment ------'
 User.create(
   username: 'user1',
   email: 'email@example.com',
-  password: 'password'
+  password: 'password',
+  country_code: Faker::Address.country_code,
+  city: Faker::Address.city
 )
 
-Kit.create(
-  name: Faker::Educator.campus,
-  description: Faker::Lorem.sentence(5)
-)
+Kit.create(name: 'Making Sense WAAG #1', description: 'AQM sensor by WAAG')
+3.times do
+  Kit.create(
+    name: Faker::Educator.campus,
+    description: Faker::Lorem.sentence(5)
+  )
+end
 
 Measurement.create(
   [
-    { name: 'air temperature',  description: 'How hot is the air' },
-    { name: 'light',  description: 'Lux is a measure' }
+    { name: 'air temperature',  description: 'How hot is the air', unit: 'C' },
+    { name: 'light',  description: 'Lux is a measure', unit: 'lux' }
   ]
 )
 
 Sensor.create(
-  name: 'sensor1',
-  description: 'sens descript'
+  [
+    {
+      name: 'My temp sensor',
+      unit: 'temperature unit',
+      measurement: Measurement.first,
+      description: 'temp sens descript'
+    },
+    {
+      name: 'My light sensor',
+      unit: 'light unit',
+      measurement: Measurement.second,
+      description: 'light sens descript'
+    }
+  ]
 )
+
+#TODO: belongs_to :board, polymorphic: true
+Component.create(
+  # TODO: Is this correct? Is Kit a board?
+  board: Kit.first,
+  sensor: Sensor.first,
+  #board_type:
+)
+
+#device has many sensors through components
+#has_many :components, as: :board
+4.times do
+  Device.create(
+    {
+      owner: User.first,
+      name: Faker::Address.city,
+      description: Faker::Address.street_name,
+      mac_address: Faker::Internet.mac_address,
+      latitude: Faker::Address.latitude,
+      longitude: Faker::Address.longitude,
+      device_token: Faker::Crypto.sha1,
+      kit: Kit.all.sample
+    }
+  )
+end
 
 Tag.create(
   [
     { name: 'Amsterdam', description: 'SCK in Adam' },
+    { name: 'Barcelona', description: 'SCK in Barcelona' },
     { name: 'Manchester', description: 'SCK in Manchester' }
+  ]
+)
+
+DevicesTag.create(
+  [
+    { device: Device.first, tag: Tag.first },
+    { device: Device.first, tag: Tag.second },
+    { device: Device.second, tag: Tag.second }
   ]
 )
 
@@ -48,22 +99,3 @@ Tag.create(
 #  token: 'random token'
 #)
 
-#Component.create(
-#  board_id: 
-#)
-
-#device has many sensors through components
-Device.create(
-  owner: User.first,
-  name: 'device2',
-  description: 'device descript',
-  #mac_address: 'macaddress',
-#  latitude: 1.1,
-#  longitude: 1.2,
-  kit: Kit.first
-)
-
-#DevicesTag.create(
-#  device: Device.first,
-#  tag: Tag.first
-#)
