29c113889ae2467101f740711c024dc9b1951d5c
Viktor Smari <viktorsmari@gmail.com>
2018-07-02 18:12:11 +0200

    Dockerized: Minuteman uses a Redis db if supplied
    as an ENV variable

--- config/initializers/minuteman.rb
+++ config/initializers/minuteman.rb
@@ -1,7 +1,8 @@
 # $analytics = Minuteman.new
 Minuteman.configure do |config|
   # You need to use Redic to define a new Redis connection
-  config.redis = Redic.new("redis://127.0.0.1:6379/1")
+  # Use ENV var for Docker if supplied, else use default
+  config.redis = Redic.new(ENV['REDIC_URL'] || "redis://127.0.0.1:6379/1")
 
   # The prefix affects operations
   config.prefix = "Tomato"
