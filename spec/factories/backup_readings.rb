4fc3a56aa2cf05d69b136f2d90721efde9914437
Viktor Smari <viktorsmari@gmail.com>
2018-10-01 13:19:13 +0200

    Update gems, fix factory bot deprication warnings.

--- spec/factories/backup_readings.rb
+++ spec/factories/backup_readings.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :backup_reading do
-    data "MyText"
-    mac "MyString"
-    version "MyString"
-    ip "MyString"
-    created_at "2015-11-18 14:32:27"
+    data { "MyText" }
+    mac { "MyString" }
+    version { "MyString" }
+    ip { "MyString" }
+    created_at { "2015-11-18 14:32:27" }
   end
 
 end
--- spec/factories/sensors.rb
+++ spec/factories/sensors.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :sensor do
-    name "MiCS-2710"
-    description "Metaloxide gas sensor"
-    unit "KΩ"
+    name { "MiCS-2710" }
+    description { "Metaloxide gas sensor" }
+    unit { "KΩ" }
   end
 end
