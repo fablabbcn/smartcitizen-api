e5f07fee176c4f7e1ef6ec478cac7dcc52da4f9e
Viktor Smari <viktorsmari@gmail.com>
2018-10-31 16:34:00 +0100

    Geocoder: Add google api key ENV var

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -129,7 +129,7 @@ else # prod / dev
       :use_https    => false,       # use HTTPS for lookup requests? (if supported)
       :http_proxy   => nil,         # HTTP proxy server (user:pass@host:port)
       :https_proxy  => nil,         # HTTPS proxy server (user:pass@host:port)
-      :api_key      => nil,         # API key for geocoding service
+      :api_key      => ENV['google_api_key'] || nil,         # API key for geocoding service
       :cache        => Redis.new,         # cache object (must respond to #[], #[]=, and #keys)
       :cache_prefix => "geocoder:", # prefix (string) to use for all cache keys
 
