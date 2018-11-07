4e4d1aedd4562bc262c3ec5f46b7c1de3588305b
Viktor Smári <viktorsmari@users.noreply.github.com>
2018-11-07 17:20:55 +0100

    WIP: Kairos dockerized (#111)
    * Geocoder: Add google api key ENV var
    
    * Query script accept arguments
    
    * Dockerize Kairos variables
    
    * Kairos: Use a single variable for containers
    
    App container uses this variable to connect to it via Telnet
    Kairos container uses the same variable to set the Telnet port
    
    * Disable Cassandra container in prod
    
    * Copy Cassandra settings from the original

--- query_readings.sh
+++ query_readings.sh
@@ -4,4 +4,10 @@
 
 # Gets reading for device 4
 
-curl -s http://api-staging.smartcitizen.me/v0/devices/4 | jq '.data.sensors[2].value'
+if [ "$1" = "localhost" ]; then
+  echo "Querying localhost"
+  curl -s http://localhost:3000/v0/devices/4 | jq '.data.sensors[2].value'
+else
+  echo "Querying staging"
+  curl -s http://staging.api.smartcitizen.me/v0/devices/4 | jq '.data.sensors[2].value'
+fi
---
+++ scripts/runkairos.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export ADDR_TELNET=${ADDR_TELNET:-0.0.0.0}
+export CASS_AUTH_PASS=${CASS_AUTH_PASS:-"cass_password"}
+export CASS_AUTH_USER=${CASS_AUTH_USER:-"cass_user"}
+export CASS_HOSTS=${CASS_HOSTS:-"cassandra-1"}
+export K_PASS=${K_PASS:-"kairos_password"}
+export K_USER=${K_USER:-"admin"}
+export kairos_telnet_port=${kairos_telnet_port:-4242}
+export PORT_CARBON_PICKLE=${PORT_CARBON_PICKLE:-2004}
+export PORT_CARBON_TEXT=${PORT_CARBON_TEXT:-2003}
+export PORT_HTTP=${PORT_HTTP:-8080}
+export READ_CONSISTENCY_DATA=${READ_CONSISTENCY_DATA:-ONE}
+export READ_CONSISTENCY_INDEX=${READ_CONSISTENCY_INDEX:-ONE}
+export READ_CONSISTENCY_INDEX=${WRITE_CONSISTENCY_INDEX:-QUORUM}
+export REPFACTOR=${REPFACTOR:-1}
+export WRITE_CONSISTENCY_DATA=${WRITE_CONSISTENCY_DATA:-QUORUM}
+
+function main {
+  echo "---- kairosdb.properties ----"
+  envsubst < /tmp/kairosdb.properties > /opt/kairosdb/conf/kairosdb.properties
+  cat /opt/kairosdb/conf/kairosdb.properties
+  echo "---- end kairosdb.properties ----"
+  /opt/kairosdb/bin/kairosdb.sh run
+}
+
+main "$@"
