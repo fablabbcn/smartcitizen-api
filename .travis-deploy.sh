e3dde3cec2204659626e431e6ab94c2468cb1741
Viktor Smari <viktorsmari@gmail.com>
2018-11-14 11:17:57 +0100

    Travis no keyscan

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -9,5 +9,5 @@ chmod 600 deploy_rsa
 ssh-add deploy_rsa
 
 #ssh -p $PORT $STAGING  "cd smartcitizen-api; ./scripts/deploy.sh"
-ssh-keyscan -p$PORT $SERVER
+#ssh-keyscan -p$PORT $SERVER
 ssh -oStrictHostKeyChecking=no -p$PORT $SERVER touch file.txt
