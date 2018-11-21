53f5e6d7f40e7e73e382cce8ca807d2940d8689b
Viktor Smari <viktorsmari@gmail.com>
2018-11-21 17:59:24 +0100

    Swap log output, first IP, then uuid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
 
   # Settings specified here will take precedence over those in config/application.rb.
-  config.log_tags = [:uuid, :remote_ip]
+  config.log_tags = [:remote_ip, :uuid]
   # Code is not reloaded between requests.
   config.cache_classes = true
 
