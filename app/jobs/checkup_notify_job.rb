03cfae38cf944d6a72fa0d5b86c98325e973ab92
Viktor Smari <viktorsmari@gmail.com>
2018-11-21 17:39:16 +0100

    Jobs: add checkups to a log/checkup.log

--- app/jobs/checkup_notify_job.rb
+++ app/jobs/checkup_notify_job.rb
@@ -2,6 +2,9 @@ class CheckupNotifyJob < ApplicationJob
   queue_as :default
 
   def perform(errormsg)
+    checkups_log = Logger.new('log/checkup.log')
+    checkups_log.level = Logger::ERROR
+    checkups_log.error(errormsg)
     # TODO: send us warnings on email / slack / grafana?
   end
 end
