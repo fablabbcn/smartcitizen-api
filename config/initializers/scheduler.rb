3cf41a5dccf440c80cb435fc6b70e69a0900bb7d
Viktor Smari <viktorsmari@gmail.com>
2018-11-22 17:36:42 +0100

    Scheduler: don't run inside rake console

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -5,19 +5,25 @@ require 'rufus-scheduler'
 #
 s = Rufus::Scheduler.singleton
 
-s.every '1m' do
-  # debug
-  #Rails.logger.info "hello, it's #{Time.now}"
-  #Rails.logger.flush
-end
 
-s.every '5m' do
-  CheckBatteryLevelBelowJob.perform_now
-  CheckDeviceStoppedPublishingJob.perform_now
-end
+unless defined?(Rails::Console) || File.split($0).last == 'rake'
+  # Only when NOT inside rake task or console
 
-s.every '1d' do
-  CheckupUserEmailBlankJob.perform_now
-  DeleteArchivedDevicesJob.perform_now
-  DeleteArchivedUsersJob.perform_now
+  s.every '1m' do
+    # debug
+    #Rails.logger.info "hello, it's #{Time.now}"
+    #Rails.logger.flush
+  end
+
+  s.every '5m' do
+    CheckBatteryLevelBelowJob.perform_now
+    CheckDeviceStoppedPublishingJob.perform_now
+  end
+
+  s.every '1d' do
+    CheckupUserEmailBlankJob.perform_now
+    DeleteArchivedDevicesJob.perform_now
+    DeleteArchivedUsersJob.perform_now
+  end
+
 end
