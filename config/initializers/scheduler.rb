88da25af498d5d1e451925a315ba0179b5b37be1
Viktor Smari <viktorsmari@gmail.com>
2019-01-22 11:57:33 +0100

    Check Device LowBat and StopedPublished every 15m

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -15,9 +15,9 @@ unless defined?(Rails::Console) || File.split($0).last == 'rake'
     #Rails.logger.flush
   end
 
-  s.every '30m' do
-    #CheckBatteryLevelBelowJob.perform_later
-    #CheckDeviceStoppedPublishingJob.perform_later
+  s.every '15m' do
+    CheckBatteryLevelBelowJob.perform_later
+    CheckDeviceStoppedPublishingJob.perform_later
   end
 
   s.every '1d' do
