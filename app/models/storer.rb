afc5ae565d2b3ad294bf8723464b0bf89a9fe80f
Pral2a <guillem@pral2a.com>
2019-01-28 16:49:13 +0100

    Simplifies websockets data-received payload

--- app/models/storer.rb
+++ app/models/storer.rb
@@ -2,26 +2,17 @@ class Storer
   include DataParser::Storer
 
   def initialize device, reading, do_update = true
-    stored = true
     @device = device
     begin
       parsed_reading = Storer.parse_reading(@device, reading)
 
-      #Kairos.http_post_to("/datapoints", parsed_reading[:_data])
-
-      #NOTE: If you want to use the Telnet port below, make sure it is open!
-      Redis.current.publish('telnet_queue', parsed_reading[:_data].to_json)
+      kairos_publish(parsed_reading[:_data])
 
       update_device(parsed_reading[:parsed_ts], parsed_reading[:sql_data]) if do_update
 
-      ts = parsed_reading[:ts]
-      readings = parsed_reading[:readings]
     rescue Exception => e
       Raven.capture_exception(e)
-      stored = false
     end
-
-    redis_publish(readings, ts, stored)
 
     raise e unless e.nil?
   end
@@ -32,19 +23,19 @@ class Storer
     #return if parsed_ts < @device.last_recorded_at
     sql_data = @device.data.present? ? @device.data.merge(sql_data) : sql_data
     @device.update_columns(last_recorded_at: parsed_ts, data: sql_data, state: 'has_published')
+    ws_publish()
   end
 
-  def redis_publish(readings, ts, stored)
-    return unless Rails.env.production? and @device
+  def kairos_publish(reading_data)
+    #Kairos.http_post_to("/datapoints", reading_data)
+    #NOTE: If you want to use the Telnet port below, make sure it is open!
+    Redis.current.publish('telnet_queue', reading_data.to_json)
+  end
+
+  def ws_publish()
+    return if Rails.env.test? or @device.blank?
     begin
-      Redis.current.publish("data-received", {
-        device_id: @device.id,
-        device: JSON.parse(@device.to_json(only: [:id, :name, :location])),
-        timestamp: ts,
-        readings: readings,
-        stored: stored,
-        data: JSON.parse(ActionController::Base.new.view_context.render( partial: "v0/devices/device", locals: {device: @device, current_user: nil}))
-      }.to_json)
+      Redis.current.publish("data-received", ActionController::Base.new.view_context.render( partial: "v0/devices/device", locals: {device: @device, current_user: nil}))
     rescue
     end
   end
