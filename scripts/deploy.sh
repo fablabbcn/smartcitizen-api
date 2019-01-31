2233460bc448c3f79d73d59522ad14e7d81a2586
Viktor Smari <viktorsmari@gmail.com>
2019-01-31 12:04:00 +0100

    Deploy:Support receiving container names in script
    In .travis-deploy we can specify:
    master branch to deploy 'app'
    and dev branch to deploy 'app db mqtt'

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+# Always pull from master? What if staging should deploy 'dev' branch?
 git pull origin master;
 docker-compose pull auth push;
-docker-compose build && docker-compose up -d
+# Accept containers as params. Supports starting only 'app db' f.x.
+docker-compose build && docker-compose up -d $@
+
+# Do we want to auto migrate?
+# For now, we only check if migration is needed
+docker-compose exec app rake db:migrate:status
+#docker-compose exec app rake db:migrate
+
 git rev-parse HEAD >> deploy_history.txt
