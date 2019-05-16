1a42db12cbaddaf9f7993d000bff4cf76a2c8d8d
Viktor Smari <viktorsmari@gmail.com>
2019-05-16 15:16:06 +0200

    Docker backup script use docker command.
    Using docker-compose directly sometimes errored.
    
    From a blog:
    
    In theory that should work, but it won’t (at least not with Docker Compose 1.20). You’ll end up with an error of ValueError: file descriptor cannot be a negative integer (-1).
    You could simplify that by replacing $(docker-compose ps -q postgres) with the name of the container if you know it beforehand, otherwise it will dynamically grab it from whatever you have defined as the service name in your docker-compose.yml.

--- scripts/docker_backup_db.sh
+++ scripts/docker_backup_db.sh
@@ -1,3 +1,3 @@
 # BACKUP
-#docker exec -i smartcitizenapi_db_1 psql -Upostgres  < dump_latest.sql
-docker-compose exec -T db pg_dump -hlocalhost -U postgres sc_dev > dump_`date +%Y-%m-%d"_"%H_%M_%S`.sql
+docker exec -i $(docker-compose ps -q db) psql -Upostgres  < dump_latest.sql
+#docker-compose exec -T db pg_dump -hlocalhost -U postgres sc_dev > dump_`date +%Y-%m-%d"_"%H_%M_%S`.sql
--- scripts/docker_restore_db.sh
+++ scripts/docker_restore_db.sh
@@ -1,3 +1,3 @@
 # RESTORE
-#docker exec -i smartcitizenapi_db_1 psql -Upostgres  < dump_latest.sql
-docker-compose exec -T db psql -Upostgres -d sc_dev < dump_latest.sql
+docker exec -i $(docker-compose ps -q db) psql -Upostgres  < dump_latest.sql
+#docker-compose exec -T db psql -Upostgres -d sc_dev < dump_latest.sql
