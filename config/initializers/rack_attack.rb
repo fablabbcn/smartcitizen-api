c922a10d8b197192a823824bda30752021919827
Viktor Smari <viktorsmari@gmail.com>
2019-07-08 17:06:01 +0200

    Tests for throttling

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,6 +1,11 @@
-Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new # defaults to Rails.cache
-
 class Rack::Attack
+
+  if Rails.env.development?
+    # In environments/development.rb, config.cache_store = :null_store
+    # Without a 'normal' cache it cannot count how many times a request has been made.
+    # Instead we manually configure this cache for development mode:
+    Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
+  end
 
   throttle('Throttle by IP', limit: 100, period: 1.minute) do |request|
     request.ip
---
+++ spec/requests/v0/rack_attack_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe 'throttle' do
+  before(:each) do
+    Rails.cache.clear
+  end
+
+  let(:limit) { 100 } # Should be the same as limit: in init/rack_attack
+
+  context "number of requests is lower than the limit" do
+    it "does not change the request status" do
+      limit.times do
+        get "/", params: {}, headers:{"REMOTE_ADDR" => "1.2.3.4"}
+        expect(response.status).to_not eq(429)
+      end
+    end
+  end
+
+  context "number of requests is higher than the limit" do
+    it "changes the request status to 429" do
+      (limit + 5).times do |i|
+        get "/", params: {}, headers:{ "REMOTE_ADDR" => "1.2.3.5"}
+
+        #p "#{i} - #{limit}"
+        if i >= limit
+          #over the limit, being throttled
+          expect(response.status).to eq(429)
+        else
+          expect(response.status).to eq(200)
+        end
+      end
+    end
+  end
+end
