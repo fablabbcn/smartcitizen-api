cc938fb0900fc4d11cfa00753f91a16a9b8ebb60
Viktor Smari <viktorsmari@gmail.com>
2019-11-13 09:37:17 +0100

    Reorder travis script
    before_install was in 2 places, but only 1 got run

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -3,6 +3,9 @@ set -e
 
 echo "#### Running Deploy Script ####"
 
+# This was moved from .travis.yml before_install section, because it breaks on PRs, because PRs dont support encrypted variables
+openssl aes-256-cbc -K $encrypted_7d0a84ef0065_key -iv $encrypted_7d0a84ef0065_iv -in deploy_rsa.enc -out deploy_rsa -d
+
 # setup ssh-agent and provide the GitHub deploy key
 eval "$(ssh-agent -s)"
 chmod 600 deploy_rsa
