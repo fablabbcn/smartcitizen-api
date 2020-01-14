6e07602fb6b6118f895b843b945733fd2040cda9
Viktor Smari <viktorsmari@gmail.com>
2020-01-14 09:33:06 +0100

    Rotate production.log file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,8 @@ Rails.application.configure do
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  else
+    config.logger = ActiveSupport::Logger.new('log/production.log', 5, 20.megabytes)
   end
 
   # Do not dump schema after migrations.
