2b95b26e9b6e0bdb1773364dcda4f5e94c6b3771
Viktor Smari <viktorsmari@gmail.com>
2020-12-15 19:52:51 +0100

    Do not run rufus in test / dev environment.

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -6,25 +6,24 @@ require 'rufus-scheduler'
 s = Rufus::Scheduler.singleton
 
 
-unless defined?(Rails::Console) || File.split($0).last == 'rake'
-  # Only when NOT inside rake task or console
+# Only when NOT inside rake task or console
+return if defined?(Rails::Console) || Rails.env.development? || Rails.env.test? || File.split($0).last == 'rake'
 
-  s.every '1m' do
-    # debug
-    #Rails.logger.info "hello, it's #{Time.now}"
-    #Rails.logger.flush
-  end
 
-  s.every '1h' do
-    CheckBatteryLevelBelowJob.perform_later
-    CheckDeviceStoppedPublishingJob.perform_later
-  end
+s.every '1m' do
+  # debug
+  #Rails.logger.info "hello, it's #{Time.now}"
+  #Rails.logger.flush
+end
 
-  s.every '1d' do
-    CheckupUserEmailBlankJob.perform_later
-    DeleteArchivedDevicesJob.perform_later
-    DeleteArchivedUsersJob.perform_later
-    DeleteOrphanedDevicesJob.perform_later
-  end
+s.every '1h' do
+  CheckBatteryLevelBelowJob.perform_later
+  CheckDeviceStoppedPublishingJob.perform_later
+end
 
+s.every '1d' do
+  CheckupUserEmailBlankJob.perform_later
+  DeleteArchivedDevicesJob.perform_later
+  DeleteArchivedUsersJob.perform_later
+  DeleteOrphanedDevicesJob.perform_later
 end
