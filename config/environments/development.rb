b9c3ec451ad3f4895370bba0c2939161d429b167
Viktor Smari <viktorsmari@gmail.com>
2021-01-07 17:34:13 +0100

    Run rails app:update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for options)
+  # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon
   #config.active_storage.service = :local
 
@@ -46,7 +46,7 @@ Rails.application.configure do
   config.active_record.verbose_query_logs = true
 
 
-  # Raises error for missing translations
+  # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
