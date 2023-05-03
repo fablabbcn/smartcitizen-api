b61d1a66abdd8fc27cb3c1b88723fe520204212d
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2023-05-03 08:27:25 +0200

    failing tests for lifting throttling limits for admins and researchers

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,18 @@
 FactoryBot.define do
-
   factory :user do
     uuid { SecureRandom.uuid }
     sequence(:username) { |n| "user#{n}" }
     sequence(:email) { |n| "user#{n}@bitsushi.com" }
     password { "password1" }
     url { "http://www.yahoo.com" }
+    role_mask { 0 }
 
     factory :admin do
       role_mask { 5 }
     end
-  end
 
+    factory :researcher do
+      role_mask { 2 }
+    end
+  end
 end
