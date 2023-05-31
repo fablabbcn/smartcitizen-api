c54d09e01f23593897c5e1a3d203367607c1ebc7
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2023-05-31 11:26:53 +0200

    get cloudflare header name right

--- config/initializers/request_cloudflare_ip.rb
+++ config/initializers/request_cloudflare_ip.rb
@@ -1,6 +1,6 @@
 class ActionDispatch::Request
     alias_method :original_remote_ip, :remote_ip
     def remote_ip
-        headers["CF_CONNECTING_IP"] || original_remote_ip
+        headers["HTTP_CF_CONNECTING_IP"] || original_remote_ip
     end
 end
