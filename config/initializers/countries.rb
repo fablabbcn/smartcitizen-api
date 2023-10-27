0b4f15b197a1c8320ecc30025d51c8efdee89c96
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2023-10-27 08:45:25 +0200

    Add Country data for Kosovo

---
+++ config/initializers/countries.rb
@@ -0,0 +1,14 @@
+ISO3166::Data.register(
+  alpha2: 'XK',
+  alpha3: 'XKX',
+  name: 'Kosovo',
+  continent: "Europe",
+  currency_code: 'EUR',
+  geo: {
+    latitude: 42.602636,
+    longitude: 20.902977,
+  },
+  translations: {
+    en: 'Kosovo'
+  }
+)
