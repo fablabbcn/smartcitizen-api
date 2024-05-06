1760d33fb1d7a83530dbccdd4221792f50dd71b4
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-05-06 06:49:57 +0200

    seperate queue for mqtt retries, with custom backoff schedule

--- app/jobs/retry_mqtt_message_job.rb
+++ app/jobs/retry_mqtt_message_job.rb
@@ -1,5 +1,17 @@
 class RetryMQTTMessageJob < ApplicationJob
-  queue_as :default
+  queue_as :mqtt_retry
+
+  sidekiq_retry_in do |count|
+   case count
+   when 0..10 # Every 30 seconds for the first 5 minutes
+    30.seconds
+   when 11..55 # Then every minute for an hour
+     1.minute
+   else
+     false # Fallback to default backoff after an hour,
+       # see https://github.com/sidekiq/sidekiq/issues/2338
+   end
+  end
 
   def perform(topic, message)
     result = MqttMessagesHandler.handle_topic(topic, message, false)
