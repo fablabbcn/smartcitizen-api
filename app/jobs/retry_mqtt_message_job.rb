4e9775ed42cecec21b29f30c42e239022e9da228
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-05-13 08:11:11 +0200

    use activejob apis to express retry timeout logic

--- app/jobs/retry_mqtt_message_job.rb
+++ app/jobs/retry_mqtt_message_job.rb
@@ -1,22 +1,25 @@
 class RetryMQTTMessageJob < ApplicationJob
+  class RetryMessageHandlerError < RuntimeError
+  end
   queue_as :mqtt_retry
 
-  sidekiq_retry_in do |count|
-   case count
-   when 0..12
-     5.seconds
-   when 12..20 # Every 30 seconds for the first 5 minutes
-    30.seconds
-   when 20..75 # Then every minute for an hour
-     1.minute
-   else
-     :discard
-   end
-  end
+
+  retry_on RetryMessageHandlerError, attempts: 75, wait: ->(count) {
+    case count
+    when 0..12
+      5.seconds
+    when 12..20 # Every 30 seconds for the first 5 minutes
+      30.seconds
+    when 20..75 # Then every minute for an hour
+      1.minute
+    else
+      :polynomially_longer
+    end
+  }
 
   def perform(topic, message)
     result = MqttMessagesHandler.handle_topic(topic, message, false)
-    raise "Message handler returned nil, retrying" if result.nil?
+    raise RetryMessageHandlerError if result.nil?
   end
 end
 
