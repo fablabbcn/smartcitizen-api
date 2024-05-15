e4e3aacb7161a8e53c4088a8df722eca9dae9dfb
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-05-15 14:21:51 +0200

    ensure retry jobs abandoned after an hour

--- app/jobs/retry_mqtt_message_job.rb
+++ app/jobs/retry_mqtt_message_job.rb
@@ -1,21 +1,22 @@
 class RetryMQTTMessageJob < ApplicationJob
   class RetryMessageHandlerError < RuntimeError
   end
+
   queue_as :mqtt_retry
 
 
-  retry_on RetryMessageHandlerError, attempts: 75, wait: ->(count) {
+  retry_on(RetryMessageHandlerError, attempts: 75, wait: ->(count) {
     case count
     when 0..12
       5.seconds
     when 12..20 # Every 30 seconds for the first 5 minutes
       30.seconds
-    when 20..75 # Then every minute for an hour
+    else # Then every minute for an hour
       1.minute
-    else
-      :polynomially_longer
     end
-  }
+  }) do |_job, _exeception|
+    # No-op, this block ensures the exception isn't reraised and retried by Sidekiq
+  end
 
   def perform(topic, message)
     result = MqttMessagesHandler.handle_topic(topic, message, false)
