eb83da1ae7a81f1e89bdea13751a09f0550e9138
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-07-04 08:50:26 +0200

    Remove deprecated avatars and uploads implementation

--- spec/controllers/v0/me_controller_spec.rb
+++ spec/controllers/v0/me_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe V0::MeController do
-  skip { is_expected.to permit(:email,:username,:password,:city,:country_code,:url,:avatar,:avatar_url).for(:update) }
+  skip { is_expected.to permit(:email,:username,:password,:city,:country_code,:url).for(:update) }
 end
