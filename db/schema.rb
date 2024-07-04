3af0a45b27f65cf83f5895d78b1d0b6f32f8db34
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-07-04 08:50:26 +0200

    Remove deprecated avatars and uploads implementation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2024_08_12_081108) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
   enable_extension "hstore"
