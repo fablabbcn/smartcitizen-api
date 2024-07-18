c6c342e78d88a465fadb3ba2274fd6c9836683d8
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-07-18 07:31:59 +0200

    fix device archived_at null bug

--- spec/jobs/delete_archived_devices_job_spec.rb
+++ spec/jobs/delete_archived_devices_job_spec.rb
@@ -13,8 +13,12 @@ RSpec.describe DeleteArchivedDevicesJob, type: :job do
     it "should delete all archived devices, archived_at at least 24 hours ago" do
       deviceNormal = create(:device, name: "dontDeleteMe", created_at: 6.weeks.ago, components: [create(:component)])
       deviceArchived = create(:device, name: "deleteMe", created_at: 1.month.ago, components: [create(:component)])
+      deviceArchivedNullArchivedAt = create(:device, name: "dontdeleteMe", created_at: 1.month.ago, components: [create(:component)])
       deviceArchivedToday = create(:device, name: "dontDeleteMe", created_at: 2.months.ago, components: [create(:component)])
       deviceArchived.archive!
+      deviceArchivedNullArchivedAt.archive!
+      deviceArchivedNullArchivedAt.archived_at = nil # A data inconsistency preseent in production that causes the job to fail
+      deviceArchivedNullArchivedAt.save!
       deviceArchivedToday.archive!
       deviceArchived.update!({archived_at: 2.days.ago})
       expect {
@@ -23,6 +27,7 @@ RSpec.describe DeleteArchivedDevicesJob, type: :job do
       expect(Device.unscoped).to include(deviceNormal)
       expect(Device.unscoped).not_to include(deviceArchived)
       expect(Device.unscoped).to include(deviceArchivedToday)
+      expect(Device.unscoped).to include(deviceArchivedNullArchivedAt)
     end
   end
 end
