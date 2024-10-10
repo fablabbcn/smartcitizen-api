e1ee360ef6ff58c012d96228aea6a008271e04ee
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-10-10 08:50:59 +0200

    use transactional fixtures in tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,6 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require "database_cleaner/active_record"
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -38,18 +37,6 @@ RSpec.configure do |config|
     # a real object. This is generally recommended, and will default to
     # `true` in RSpec 4.
     mocks.verify_partial_doubles = true
-  end
-
-  DatabaseCleaner.strategy = :truncation
-
-  config.before(:suite) do
-    DatabaseCleaner.clean
-  end
-
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
   end
 
 # The settings below are suggested to provide a good initial experience
