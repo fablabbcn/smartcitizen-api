ae77b3d092b453d3eaf34f9d3a5c4612829a2f90
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-10-29 06:16:13 +0100

    rip out old styles and add bootstrap in assets pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery3
 //= require jquery_ujs
 //= require turbolinks
+//= require popper
+//= require bootstrap-sprockets
 //= require_tree .
