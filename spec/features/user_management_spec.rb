1b14357fdc0657bcfa9b9dd378b94ac777c40731
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-11-05 19:17:19 +0100

    responsive tweaks, link back to login from signup

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -5,11 +5,11 @@ feature "User signs up for an account" do
     visit "/login"
     click_on "Sign up"
     expect(page).to have_current_path(new_ui_user_path)
-    fill_in "Username", with: "myusername"
     fill_in "Email", with: "user@example.com"
+    fill_in "Username", with: "myusername"
     fill_in "Password", with: "password123"
     fill_in "Password confirmation", with: "password123"
-    check "I have read and accept the Terms and Conditions"
+    check "I accept the Terms and Conditions"
     click_on "Sign up"
     expect(page).to have_content("Thanks for signing up! You are now logged in.")
   end
