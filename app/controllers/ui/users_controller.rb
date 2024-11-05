8a506df7924283c09e30697c30435813760e35b1
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-11-05 21:57:17 +0100

    logged in users can't create new users

--- app/controllers/ui/users_controller.rb
+++ app/controllers/ui/users_controller.rb
@@ -6,11 +6,21 @@ module Ui
     end
 
     def new
+      if current_user
+        flash[:alert] = I18n.t(:new_user_not_allowed_for_logged_in_users)
+        redirect_to ui_users_path
+        return
+      end
       @title = I18n.t(:new_user_title)
       @user = User.new
     end
 
     def create
+      if current_user
+        flash[:alert] = I18n.t(:new_user_not_allowed_for_logged_in_users)
+        redirect_to ui_users_path
+        return
+      end
       @user = User.new(params.require(:user).permit(
         :username,
         :email,
