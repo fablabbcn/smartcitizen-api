9a5cb3c23819586ca5d345994908eac4ddbd0d3d
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-11-05 18:51:24 +0100

    Happy path for user signup

--- app/controllers/ui/users_controller.rb
+++ app/controllers/ui/users_controller.rb
@@ -4,5 +4,23 @@ module Ui
     def index
       @title = I18n.t(:users_index_title)
     end
+
+    def new
+      @title = I18n.t(:new_user_title)
+      @user = User.new
+    end
+
+    def create
+      @user = User.create(params.permit(
+        :username,
+        :email,
+        :password,
+        :password_confirmation,
+        :ts_and_cs,
+      ))
+      session[:user_id] = @user.id
+      flash[:success] = I18n.t(:new_user_success)
+      redirect_to ui_users_path
+    end
   end
 end
