e8881a28588fa851e466e453c60926c889980585
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-12-12 22:23:19 +0100

    breadccrumb navigation

--- app/controllers/ui/application_controller.rb
+++ app/controllers/ui/application_controller.rb
@@ -2,5 +2,25 @@ module Ui
   class ApplicationController < ActionController::Base
     layout "application"
     include SharedControllerMethods
+
+    def add_breadcrumbs(*crumbs)
+      crumbs.each do |crumb|
+        add_breadcrumb(*crumb)
+      end
+    end
+
+    def add_breadcrumb(label, url=nil)
+      breadcrumbs << Breadcrumb.new(breadcrumbs, label, url)
+    end
+
+    def breadcrumbs
+      unless @breadcrumbs
+        @breadcrumbs = []
+        @breadcrumbs << Breadcrumb.new(@breadcrumbs, t(:root_breadcrumb))
+      end
+      return @breadcrumbs
+    end
+
+    helper_method :breadcrumbs
   end
 end
