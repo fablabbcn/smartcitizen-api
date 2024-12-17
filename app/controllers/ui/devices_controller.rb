6d4092311ba820a07f241738d6dd8a8a87988e8e
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-12-17 11:34:14 +0100

    beginnings of device profile w/ reusable maps and metadata components

--- app/controllers/ui/devices_controller.rb
+++ app/controllers/ui/devices_controller.rb
@@ -3,7 +3,10 @@ module Ui
     def show
       find_device!
       @title = I18n.t(:show_device_title, name: @device.name)
-      add_breadcrumb(@title, ui_device_path(@device.id))
+      add_breadcrumbs(
+        [I18n.t(:show_user_title, owner: possessive(@device.owner, current_user)), ui_user_path(@device.owner.username)],
+        [@title, ui_device_path(@device.id)]
+      )
       render "show", layout: "base"
     end
 
