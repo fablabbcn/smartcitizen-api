991810788654a4071967c23e2f5b7cc5922d4c24
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-12-18 19:27:59 +0100

    map location picker for devices

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,12 @@
 import * as $ from "jquery";
 import {setupCopyableInputs} from "components/copyable_input";
 import {setupDeviceMaps} from "components/device_map";
+import {setupMapLocationPickers} from "components/map_location_picker";
 
 export default function setupApplication() {
   $(function() {
     setupCopyableInputs();
     setupDeviceMaps();
+    setupMapLocationPickers();
   });
 }
