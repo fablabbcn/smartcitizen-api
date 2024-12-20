74ca0d3e58bbfa6166d3821dd113ab04dbeca5b6
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-12-20 22:30:40 +0100

    maybe we don't need to set up the dev database at all?

--- ci.sh
+++ ci.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -Eeuo pipefail
 git config --global --add safe.directory /app
-bundle exec bin/rake db:create
-bundle exec bin/rake db:schema:load
 unset DATABASE_URL
 bundle exec bin/rake spec
