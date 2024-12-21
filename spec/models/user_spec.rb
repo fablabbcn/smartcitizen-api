dd2b894fe5fb9214c1b99d0ee80787dc46855ac8
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-12-21 08:01:14 +0100

    add a failing test to make sure it's actually working

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
 
+
+  it "is expected to break CI" do
+    expect(false).to be_true
+  end
+
   it { is_expected.to have_secure_password }
   # it { is_expected.to validate_presence_of(:first_name) }
   # it { is_expected.to validate_presence_of(:last_name) }
