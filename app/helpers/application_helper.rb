0bdab2244eaaca04194e0f072be4c24ec2eb9038
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2025-04-04 11:53:04 +0200

    red hover on signout button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,6 +16,7 @@ module ApplicationHelper
 
   def sc_nav_button_to(legend, path, opts={})
     button_class = opts[:dark_buttons] ? "btn-dark" : "btn-secondary"
+    button_class << (" " + opts[:class]) if opts[:class]
     button_class << " btn-active" if current_page?(path)
     link_to(legend, path, class: "btn #{button_class} me-md-2 mb-3 w-100 w-md-auto")
   end
